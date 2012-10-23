360c6e2440b309ea04de4b3cb644f6cbdccf8e6f
Petr Hosek <p.hosek@imperial.ac.uk>
2012-10-23 16:21:47 +0100

    Python 3 compatibility

--- jellyfish.h
+++ jellyfish.h
@@ -12,19 +12,35 @@
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #endif
 
+/* Python 3 support */
+#if PY_MAJOR_VERSION >= 3
+  #define PyInt_AsLong PyLong_AsLong
+  #define PyInt_Check PyLong_Check
+  #define PyInt_FromLong PyLong_FromLong
+  #define PyString_AS_STRING PyBytes_AS_STRING
+  #define PyString_AsString PyBytes_AsString
+  #define PyString_AsStringAndSize PyBytes_AsStringAndSize
+  #define PyString_Check PyBytes_Check
+  #define PyString_FromString PyBytes_FromString
+  #define PyString_FromStringAndSize PyBytes_FromStringAndSize
+  #define PyString_Size PyBytes_Size
+#endif
+
+#if PY_MAJOR_VERSION == 2
+  #define to_path(x) to_bytes(x)
+  #define to_encoding(x) to_bytes(x)
+#else
+  #define to_path(x) to_unicode(x, Py_FileSystemDefaultEncoding, "strict")
+  #define to_encoding(x) PyUnicode_DecodeASCII(x, strlen(x), "strict")
+#endif
+
 double jaro_winkler(const char *str1, const char *str2, bool long_tolerance);
 double jaro_distance(const char *str1, const char *str2);
-
 size_t hamming_distance(const char *str1, const char *str2);
-
 int levenshtein_distance(const char *str1, size_t len1, const char *str2, size_t len2);
-
 int damerau_levenshtein_distance(const char *str1, const char *str2);
-
 char* soundex(const char *str);
-
 char* metaphone(const char *str);
-
 char *nysiis(const char *str);
 
 char* match_rating_codex(const char *str);
